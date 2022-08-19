bc6bd52e51916b8584a681ac84ba26e26d9784db
Jazyel99 <jaziel@outlook.com>
2022-08-18 23:56:21 -0300

    style: add screens break-points

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,13 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'sx': '480px',
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-      
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    }
+      '2xs' : '320px',
+      'xs' : '360px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 }
